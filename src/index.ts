072c9fa222b05811db6f83bd570868abac048b19
Yifei Teng <teng9@illinois.edu>
2016-10-31 20:43:00 -0500

    terminal

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import swimFuture from './swim';
 import { Command, Exit, runConsole } from './command';
 
+let isSwimActive = false;
+
 runConsole(item => {
   switch (item.kind) {
     case 'command':
@@ -8,11 +10,19 @@ runConsole(item => {
     break;
     case 'exit':
       console.log('Shutting down');
+      if (isSwimActive) {
+        swimFuture
+        .then(swim => swim.leave())
+        .then(() => process.exit(0));
+      } else {
+        process.exit(0);
+      }
     break;
   }
 });
 
 swimFuture.then(swim => {
+  isSwimActive = true;
   console.log('Membership protocol ready');
   // initialize file protocol
 });
