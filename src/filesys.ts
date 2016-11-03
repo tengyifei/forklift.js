eaf813aa4c43fdff2b922d33686050f5ff9c16ee
Yifei Teng <teng9@illinois.edu>
2016-11-02 20:11:09 -0500

    key probing

--- src/filesys.ts
+++ src/filesys.ts
@@ -3,6 +3,8 @@ import swimFuture from './swim';
 import * as Swim from 'swim';
 import * as rp from 'request-promise';
 import * as Bluebird from 'bluebird';
+import * as crypto from 'crypto';
+const modexp = require('mod-exp');
 
 export const fileSystemProtocol = swimFuture.then(async swim => {
     interface Dictionary {
@@ -67,6 +69,26 @@ export const fileSystemProtocol = swimFuture.then(async swim => {
     let ls = (key: string) => new Promise((resolve, reject) => {
 
     });
+
+    // probing strategy for a node
+    const hashKey = (key: string) => function*() {
+      const md5sum = crypto.createHash('md5');
+      md5sum.update(key);
+      let digestHex = md5sum.digest('hex');
+      // squash to 16-bit number
+      let exponent = 0;
+      for (let i = 0; i < 32; i += 4) {
+        let part = +digestHex.substr(i, 4);
+        exponent = exponent ^ part;
+      }
+      // generate probes
+      for (let i = 0; i < 10; i++) {
+        // (7 ^ (exponent + i)) mod 10
+        yield <number> modexp(7, exponent + i, 10);
+      }
+    };
+
+    // replicate when nodes fail
 
     let store = () => Promise.resolve(Object.keys(files));
 
