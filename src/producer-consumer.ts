815637d593211b654645e6bfc5eeda8a1e19b609
Yifei Teng <tengyifei88@gmail.com>
2016-11-29 14:36:52 -0600

    reactive queue

---
+++ src/producer-consumer.ts
@@ -0,0 +1,44 @@
+import { Observable, ReplaySubject } from 'rxjs/Rx';
+
+class ReactiveQueue <T> {
+  private stream: ReplaySubject<T>;
+  private queue: T[];
+  private waiting: boolean;
+  private listeners: ((x: T) => PromiseLike<void>)[];
+
+  constructor() {
+    this.stream = new ReplaySubject<T>();
+    this.queue = [];
+    this.listeners = [];
+    this.waiting = false;
+  }
+
+  subscribe <R> (onNext: (x: T) => PromiseLike<void>) {
+    this.listeners.push(onNext);
+    this.stream.subscribe(t => {
+      // wait for all consumers
+      Promise.all(this.listeners.map(l => l(t)))
+      .then(_ => {
+        // consumers have processed current element
+        this.queue.shift();
+        // trigger next element if possible
+        if (this.queue.length > 0) {
+          this.waiting = true;
+          this.stream.next(this.queue[0]);
+        } else {
+          this.waiting = false;
+        }
+      });
+    });
+  }
+
+  push(t: T) {
+    this.queue.push(t);
+    if (this.waiting) {
+      // if consumer is busy, cache it and do nothing
+    } else {
+      this.waiting = true;
+      this.stream.next(t);
+    }
+  }
+}
