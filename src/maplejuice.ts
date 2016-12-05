53bb60984e4d5b43a2baf45d422f7333996daf49
Yifei Teng <tengyifei88@gmail.com>
2016-12-05 09:44:02 -0600

    debug

--- src/maplejuice.ts
+++ src/maplejuice.ts
@@ -740,15 +740,15 @@ export const maplejuice = Promise.all([paxos, fileSystemProtocol, swimFuture])
             concurrentStreams[x][1] += 1;
             return concurrentStreams[x][0][index];
           }
-          // not found
-          let newStream = partitionDataset(sourceDirectory, numMaples);
-          // exhaust stream up to index - 1
-          for (let x = 0; x <= index - 1; x++) {
-            newStream[x].on('data', (chunk) => {});
-          }
-          concurrentStreams.push([newStream, index + 1]);
-          return newStream[index];
         }
+        // not found
+        let newStream = partitionDataset(sourceDirectory, numMaples);
+        // exhaust stream up to index - 1
+        for (let x = 0; x <= index - 1; x++) {
+          newStream[x].on('data', (chunk) => {});
+        }
+        concurrentStreams.push([newStream, index + 1]);
+        return newStream[index];
       })(i));
     }
     // start job
