9b565d7372671bc574227b8aa5a1a006ffe01a6a
Yifei Teng <tengyifei88@gmail.com>
2016-12-05 10:40:14 -0600

    debug

--- src/swim.ts
+++ src/swim.ts
@@ -21,10 +21,10 @@ export default bootstrapDNS.then(() => new Promise<Swim>((resolve, reject) => {
     },
     codec: 'msgpack', // optional
     disseminationFactor: 15, // optional
-    interval: 450, // optional
+    interval: 750, // optional
     joinTimeout: 200, // optional
-    pingTimeout: 150, // optional
-    pingReqTimeout: 250, // optional
+    pingTimeout: 250, // optional
+    pingReqTimeout: 350, // optional
     pingReqGroupSize: 3, // optional
     udp: { maxDgramSize: 768 } // optional
   };
