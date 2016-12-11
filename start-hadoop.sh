46e49c6a36aadf970879dd9fcc9c8f411ec31cfc
Yifei Teng <tengyifei88@gmail.com>
2016-12-10 22:03:51 -0600

    final

--- start-hadoop.sh
+++ start-hadoop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-common="04 05 06 07 08 09"
+common="04 05 06 07"
 
 ssh teng9@fa16-cs425-g06-01.cs.illinois.edu '$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode'
 ssh teng9@fa16-cs425-g06-02.cs.illinois.edu '$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager'
